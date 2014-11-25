e71627a74c11e310f853532365ae33ad013ea567
iMac 8 <imac8@iMac-8.local>
2014-11-25 15:06:13 +0100

    Initial Commit

---
+++ CartasTests/CartasTests.swift
@@ -0,0 +1,36 @@
+//
+//  CartasTests.swift
+//  CartasTests
+//
+//  Created by iMac 8 on 25/11/14.
+//  Copyright (c) 2014 Javier Galardi. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class CartasTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
